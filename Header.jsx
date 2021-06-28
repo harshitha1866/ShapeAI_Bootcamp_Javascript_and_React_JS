5fade31343174d52ee65cf1de4c123456e823f3d
harshitha1866 <68456962+harshitha1866@users.noreply.github.com>
2021-06-28 16:51:45 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>MyChoice E-Book Store</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>twilight</h1>
+        <h5>By Stephenie Mayor</h5>
+        <p>
+          A 2005 young adult vampire-romance novel. The book was the
+          biggest-selling of 2008.
+        </p>
+      </div>
+      <div className="note">
+        <h1>AFTER</h1>
+        <h5>By Anna Todd</h5>
+        <p>
+          A 2014 young adult romance novel written by American author and
+          published by Gallery Books.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Harry Potter</h1>
+        <h5>By J.K Rowling</h5>
+        <p>
+          A series of seven fantasy novels written by British author. The
+          release of the first novel was in 1997.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Gabriel's Inferno</h1>
+        <h5>By Sylvian Renard</h5>
+        <p>
+          Gabriel's Inferno is an romance novel by an anonymous Canadian author.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Info;
